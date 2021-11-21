cc0640c687f7d0eebf47aedab573f3595864c676
ysKim <znxkznxk1030@gmail.com>
2021-11-21 07:19:24 +0530

    finish

--- assignments/test2/app.js
+++ assignments/test2/app.js
@@ -1,35 +1,52 @@
 const imageList = ["image1", "image2", "image3", "image4", "image5"];
+let imgUrlList = [];
 const $cardlist = document.querySelector(".card-list");
 const $prevBtn = document.querySelector(".nav-prev");
 const $nextBtn = document.querySelector(".nav-next");
+const $uploadBtn = document.getElementById("upload");
+
 let page = 0;
 let $cards = null;
-$prevBtn.addEventListener("click", (e) => {});
 
-init();
+$uploadBtn.addEventListener("change", function () {
+  if (this.files && this.files[0]) {
+    const newImgUrl = URL.createObjectURL(this.files[0]);
+    // imgUrlList.push(URL.createObjectURL(this.files[0]));
+    $cardlist.appendChild(createCard(newImgUrl));
+
+    $cards = document.querySelectorAll(".card-item");
+    const l = $cards ? $cards.length : 0;
+    page = 1;
+
+    rotateCards();
+  }
+});
 
 $prevBtn.addEventListener("click", () => {
   const l = $cards ? $cards.length : 0;
   page -= 1;
   if (page < 0) page = l;
-  display();
+  rotateCards();
 });
 
 $nextBtn.addEventListener("click", () => {
   const l = $cards ? $cards.length : 0;
   page = (page + 1) % l;
-  display();
+  rotateCards();
 });
 
+init();
+
 function init() {
-  imageList.forEach((image) => {
-    $cardlist.appendChild(createCard(image));
+  imgUrlList = imageList.map((image) => imgToUrl(image));
+  imgUrlList.forEach((imgUrl) => {
+    $cardlist.appendChild(createCard(imgUrl));
   });
 
-  display();
+  rotateCards(0);
 }
 
-function display() {
+function rotateCards() {
   $cards = document.querySelectorAll(".card-item");
   const l = $cards.length;
   let an = 0;
@@ -39,32 +56,30 @@ function display() {
   }
 
   $cards.forEach(($card, i) => {
-    let index = ((i + page) % l);
-
-    $card.style.backgroundImage = `url(img/image${index + 1}.png)`;
     $card.style.transform = `
       rotateY(${an * (i + page)}deg)
       translateZ(250px)
       rotateY(${-an * (i + page)}deg)`;
-
-    // console.log("angle" + an * index);
   });
-  
 }
 
-function createCard(image) {
+function imgToUrl(image) {
+  return `img/${image}.png`;
+}
+
+function createCard(imgUrl) {
   const $card = document.createElement("li");
   $card.classList.add("card-item");
 
   const $img = document.createElement("div");
   $img.classList.add("card-img");
-  $img.style.backgroundImage = `url(img/${image}.png)`;
+  $img.style.background = `url(${imgUrl}) no-repeat center center/cover`;
 
   const $mirror = document.createElement("div");
 
   $mirror.classList.add("card-img--mirror");
 
-  $mirror.style.background = `linear-gradient(180deg, #FFFFFF 50%, rgba(255, 255, 255, 0.71) 100%), url(img/${image}.png)`;
+  $mirror.style.background = `linear-gradient(180deg, #FFFFFF 50%, rgba(255, 255, 255, 0.71) 100%), url(${imgUrl}) no-repeat center center/cover`;
 
   $img.appendChild($mirror);
   $card.appendChild($img);
