a12d080d070eda5030a2b36ca82ff23d467d7826
ysKim <znxkznxk1030@gmail.com>
2021-11-21 17:57:03 +0530

    slide finish

--- assignments/test4/app.js
+++ assignments/test4/app.js
@@ -2,27 +2,37 @@ let page = 1
 const $nav_links = document.querySelectorAll(".nav-link")
 const $contents = document.querySelectorAll(".content > section")
 
-console.log($nav_links)
 
-window.onload = () => {
-  $nav_links.forEach(($navlink) => {
-    console.log($navlink)
-    console.log($navlink.getAttribute('data-page'))
-
-    $navlink.addEventListener("click", (e) => {
-      e.preventDefault()
-      console.log(e)
-      page = $navlink.getAttribute("data-page")
-      removeActiveLinkAll()
-      $navlink.classList.add("active")
-    })
-  })
+function init() {
+  activeLink()
+  slideContent()
 }
+
+$nav_links.forEach(($navlink) => {
+  console.log($navlink)
+  $navlink.addEventListener("click", (e) => {
+    page = $navlink.getAttribute("data-page")
+    activeLink()
+    slideContent()
+  })
+})
 
 const $nav_top = document.getElementsByClassName("link-topfive")
 
-function removeActiveLinkAll() {
+function activeLink() {
   $nav_links.forEach(($navlink) => {
     $navlink.classList.remove("active")
+    const data_page = $navlink.getAttribute("data-page")
+    if (page === data_page) {
+      $navlink.classList.add("active")
+    } else {
+      $navlink.classList.remove("active")
+    }
+  })
+}
+
+function slideContent() {
+  $contents.forEach(($content) => {
+    $content.style.transform = `translateX(-${100 * (page - 1)}vw)`
   })
 }
